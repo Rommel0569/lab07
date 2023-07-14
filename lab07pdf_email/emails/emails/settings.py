6b37a676b167779b79683910469e755559841d89
Rommel Abraham Chambi Velasquez <rchambive@unsa.edu.pe>
2023-07-14 02:16:49 -0500

    Configurando settings.py

--- lab07pdf_email/emails/emails/settings.py
+++ lab07pdf_email/emails/emails/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'send'
 ]
 
 MIDDLEWARE = [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
 TIME_ZONE = 'UTC'
 
@@ -121,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+
+EMAIL_HOST_USER ='rchambive@unsa.edu.pe'
+EMAIL_HOST_PASSWORD = 'htwggrucjwpnhdoz'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
