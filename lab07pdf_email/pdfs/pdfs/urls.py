3eae7181e6082587a9be8d58e8d998f9aec93d1f
Rommel Abraham Chambi Velasquez <rchambive@unsa.edu.pe>
2023-07-14 02:03:02 -0500

    Modificando el urls.py

--- lab07pdf_email/pdfs/pdfs/urls.py
+++ lab07pdf_email/pdfs/pdfs/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from pdf.views import GeneratePdf
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pdf/',GeneratePdf.as_view(), name='PDF'),
 ]
