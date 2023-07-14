130ef79fa55d2383b20aaa85d346016d615e4252
Rommel Abraham Chambi Velasquez <rchambive@unsa.edu.pe>
2023-07-14 02:20:00 -0500

    Condigurando el view.py para el asunto, el remitente y el destinatario

--- lab07pdf_email/emails/send/views.py
+++ lab07pdf_email/emails/send/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
+def index(request):
+    send_mail('PROBANDO CORREO DE LABORATORIOGGGAAAA',
+              'HOLA AMIGUITO COMO TAS',
+              'rchambive@unsa.edu.pe',
+              ['rommelchambi8@gmail.com'],
+              fail_silently=False)
+    return render(request, 'send/index.html')
